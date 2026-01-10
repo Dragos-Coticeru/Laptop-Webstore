0e6f81c0b026010083d7fefae35e724cbd6ecd7c
Dragos Coticeru <dragos.coticeru@gmail.com>
2026-01-10 23:02:05 +0200

    cleanup

--- app/services.py
+++ app/services.py
@@ -2,11 +2,9 @@ import pypyodbc as odbc
 
 def get_db_connection():
     DRIVER_NAME = 'SQL SERVER'
-    # Numele serverului tau
     SERVER_NAME = 'DRAGOS\SQLEXPRESS'
     DATABASE_NAME = 'Website_Database'
     
-    # Userul si parola create la pasii de mai sus
     UID = 'LaptopUser'
     PWD = '123'
 
